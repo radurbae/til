db848fbe34d0fcacacabac409e853a1ff1a725f2
Raditya Adyatma <radurbae@gmail.com>
2026-01-11 20:35:57 +0700

    chore: configure GitHub Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    /* config options here */
+    output: "export",
+    basePath: "/til_rads",
+    images: {
+        unoptimized: true,
+    },
 };
 
 export default nextConfig;
